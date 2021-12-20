f54f13127fa93cd5893822f2a3e2a5e37a094057
hebarachid <67964217+hebarachid@users.noreply.github.com>
2021-12-20 12:02:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ids_jobtask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ids_jobtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
