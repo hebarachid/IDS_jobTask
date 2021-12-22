a6d466654e6e06d256b772a71ef37e3c333b0e98
hebarachid <67964217+hebarachid@users.noreply.github.com>
2021-12-22 10:02:40 +0200

    binding data from view to adapter and using Glide library to display images

---
+++ app/src/main/java/com/example/ids_jobtask/firstAdapter.java
@@ -0,0 +1,54 @@
+package com.example.ids_jobtask;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class firstAdapter extends RecyclerView.Adapter<firstAdapter.MyViewHolder>{
+    private Context mContext;
+    private List<page1GalleryInformation> mData;
+
+    public firstAdapter(Context mContext, List<page1GalleryInformation> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        LayoutInflater inflater=LayoutInflater.from(mContext);
+        v=inflater.inflate(R.layout.gallery_first_page_items,parent,false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+      holder.Name.setText(mData.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+         TextView Name;
+         ImageView image;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Name = itemView.findViewById(R.id.Name);
+            this.image = itemView.findViewById(R.id.CroppedImage320x308);
+        }
+    }
+
+}
--- app/src/main/java/com/example/ids_jobtask/page1GalleryInformation.java
+++ app/src/main/java/com/example/ids_jobtask/page1GalleryInformation.java
@@ -1,3 +1,5 @@
+package com.example.ids_jobtask;
+
 public class page1GalleryInformation {
 
     String  Name;
